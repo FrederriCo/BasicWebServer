a85d0244bebb101924f9b5161bce658be672c65f
Ferdi Mehmedov <FredericCo@mail.bg>
2022-01-22 13:11:49 +0200

    C# WEB

---
+++ BasicWebServer.Server/HTTP/Response/HeaderCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BasicWebServer.Server.HTTP.Response
+{
+    public class HeaderCollection
+    {
+        private readonly Dictionary<string, Header> headers;
+
+        public HeaderCollection() => headers = new Dictionary<string, Header>();
+            
+        public int Count => this.headers.Count;
+
+        public void Add(string name, string value)
+        {
+            var header = new Header(name, value);
+
+            this.headers.Add(name, header);
+        }
+    }
+}
---
+++ BasicWebServer.Server/HTTP/Response/Response.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace BasicWebServer.Server.HTTP.Response
+{
+    public class Response
+    {
+        public Response(StatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+
+            this.Headers.Add("Server", "My Web Server");
+            this.Headers.Add("Date", $"{DateTime.UtcNow:r}");
+        }
+
+        public StatusCode StatusCode { get; init; }
+
+        public HeaderCollection Headers { get; } = new HeaderCollection();
+    }
+}
