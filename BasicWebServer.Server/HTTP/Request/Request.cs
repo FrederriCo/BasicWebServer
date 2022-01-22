7d0773eff028290dbc926e49a6465371b6877fb6
Ferdi Mehmedov <FredericCo@mail.bg>
2022-01-22 13:15:38 +0200

    C# WEB

--- BasicWebServer.Server/HTTP/Request/Request.cs
+++ BasicWebServer.Server/HTTP/Request/Request.cs
@@ -1,6 +1,13 @@
-﻿namespace BasicWebServer.Server.HTTP.Request
+﻿using BasicWebServer.Server.HTTP.Response;
+
+namespace BasicWebServer.Server.HTTP.Request
 {
     public class Request
     {
+        public Method Method { get; private set; }
+        public string Url { get; private set; }
+        public HeaderCollection Headers{ get; private set; }
+        public string Body { get; private set; }
+
     }
 }
