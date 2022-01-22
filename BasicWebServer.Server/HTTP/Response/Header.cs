75a6687f56ff11f8a33c8bcadd1fa9cc36101b05
Ferdi Mehmedov <FredericCo@mail.bg>
2022-01-22 12:57:47 +0200

    C# WEB

---
+++ BasicWebServer.Server/HTTP/Response/Header.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BasicWebServer.Server.HTTP.Response
+{
+    public class Header
+    {
+        public Header(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
