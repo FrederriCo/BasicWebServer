8c052ec54db75569b5b928e420cecfc59076e3a1
Ferdi Mehmedov <FredericCo@mail.bg>
2022-01-24 16:13:17 +0200

    C# WEB

--- BasicWebServer/StartUp.cs
+++ BasicWebServer/StartUp.cs
@@ -10,8 +10,8 @@ namespace BasicWebServer
     {
         public static void Main()
         {
-            var server = new HttpServer("127.0.0.0", 8080);
-            server.Start();               
+           var server = new HttpServer("127.0.0.1", 5656);
+          server.Start();               
 
 
         }
