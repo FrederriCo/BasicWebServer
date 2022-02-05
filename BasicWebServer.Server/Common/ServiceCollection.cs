0fd74aa8758232fbc8afad4eb0169419898377c9
Ferdi Mehmedov <FredericCo@mail.bg>
2022-02-05 11:37:48 +0200

    C# WEB

---
+++ BasicWebServer.Server/Common/ServiceCollection.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace BasicWebServer.Server.Common
+{
+    public class ServiceCollection : IServiceCollection
+    {
+        private readonly Dictionary<Type, Type> services;
+        public ServiceCollection()
+        {
+            services = new Dictionary<Type, Type>();
+        }
+
+        public IServiceCollection Add<TService, TImplementation>()
+            where TService : class
+            where TImplementation : TService
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceCollection Add<TService>() where TService : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public object CreateInstance(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService Get<TService>() where TService : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
